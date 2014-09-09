d242e1c4012641d3c2bd81e88d23c929d055bbe6
Xexanos <xexanos@web.de>
2014-09-09 21:02:16 +0200

    Initial commit

---
+++ src/java/net/xexanos/oilcraft/OilCraft.java
@@ -0,0 +1,28 @@
+package net.xexanos.oilcraft;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "OilCraft", name = "OilCraft", version = "1.7.10-0.0.1")
+public class OilCraft {
+
+    @Mod.Instance("OilCraft")
+    public static OilCraft instance;
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent e){
+
+    }
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent e){
+
+    }
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent e){
+
+    }
+}
